ee84fed97ab542441cc02bc75e910eb6ad2b1375
RSJR-19 <sumbadrenan619@gmail.com>
2025-07-04 15:05:35 +0800

    added js values

--- zcript.js
+++ zcript.js
@@ -2,8 +2,13 @@ let getMainWrapper = document.getElementById("main-wrapper")
 let getMainContainer = document.getElementById("main-container")
 let getOverlayOne = document.getElementById("overlay-log-one")
 let getFullScreenOverlayOne = document.getElementById("full-screen-overlay-one")
-
-
+let getFullScreenOverlayTwo = document.getElementById("full-screen-overlay-two")
+let getPurposeInput = document.getElementById("purposeInput")
+let getPriceInput = document.getElementById("priceInput")
+let purposeInputValue = "";
+let priceInputValue = 0;
+let isPurposeGiven = false;
+let isPriceGiven = false;
 
 function logExpense() {
 setTimeout(() => {

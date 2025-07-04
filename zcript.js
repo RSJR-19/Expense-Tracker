d3b3450000c9890e764658c9bd68d25c98fd3e4f
RSJR-19 <sumbadrenan619@gmail.com>
2025-07-04 21:30:54 +0800

    proceed pt.1

--- zcript.js
+++ zcript.js
@@ -5,10 +5,16 @@ let getFullScreenOverlayOne = document.getElementById("full-screen-overlay-one")
 let getFullScreenOverlayTwo = document.getElementById("full-screen-overlay-two")
 let getPurposeInput = document.getElementById("purposeInput")
 let getPriceInput = document.getElementById("priceInput")
+let getProceedBtn = document.getElementById("proceedButton");
 let purposeInputValue = "";
 let priceInputValue = 0;
 let isPurposeGiven = false;
 let isPriceGiven = false;
+getPriceInput.placeholder = `Input Gastos price...` ;
+ getPurposeInput.placeholder = `Input Gastos details...`;
+
+
+
 
 function logExpense() {
 setTimeout(() => {
@@ -34,11 +40,14 @@ isPurposeGiven = true;
 getPriceInput.focus();
 }
 else {
+  isPurposeGiven = false;
 getPurposeInput.placeholder = `Please input valid detail * `
 getPurposeInput.style.border = "3px solid red";
 
 
-}}
+}
+validateProceed();
+}
 
 getPurposeInput.addEventListener("keydown", function (event) {
 if (event.key === "Enter") {
@@ -51,30 +60,50 @@ getPurposeInput.blur()
 function enterPrice() {
 priceInputValue = getPriceInput.value;
 getPriceInput.style.border = "3px black solid";
-if(priceInputValue < 0){
+if(priceInputValue > 0){
   isPriceGiven = true;
+  
 }
 else {
+  isPriceGiven = false;
   getPriceInput.placeholder = `Please input valid amount`;
   getPriceInput.style.border = "3px red solid";
 }
-
-
-
-
+validateProceed();
 }
 
 getPriceInput.addEventListener("keydown", function (event) {
-  if (event.key = "Enter") {
+  if (event.key === "Enter") {
     enterPrice();
     getPriceInput.blur();
   }});
   
   getPurposeInput.addEventListener("focus", () => {
   getPurposeInput.style.border = "3px solid black";
+  getPurposeInput.placeholder = `Input Gastos details...`;
+  
 });
 
 
   getPriceInput.addEventListener("focus", () => {
   getPriceInput.style.border = "3px solid black";
+  getPriceInput.placeholder = `Input Gastos price...` ;
 });
+
+function validateProceed(){
+if(isPurposeGiven === true && isPriceGiven == true){
+getProceedBtn.style.backgroundColor =  " rgb(246, 223, 11)";
+}
+else {
+  getProceedBtn.style.backgroundColor = "rgb(195, 192, 168)";
+}
+}
+
+/*function logExpenseProceed(){
+if (isPurposeGiven === true && isPriceGiven == true){
+
+
+
+
+
+}*/
